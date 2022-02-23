a2a5fb33b5a538e7368bbc83a92bbcc4226fc8b6
Suzzat9 <shobithacherian@Shobithas-MacBook-Air.local>
2022-02-23 00:26:18 -0600

    added method for plotting multiple graphs

--- networks.py
+++ networks.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import math
 import pandas as pd
 
-file = 'data/adj_allVillageRelationships_HH_vilno_3.csv'
+file = 'data/adj_allVillageRelationships_HH_vilno_2.csv'
 
 leader_data = pd.read_stata("data/panel.dta", index_col="village")
 
@@ -49,6 +49,15 @@ class Vill_Net:
         self._evcent_round = [round(ev, 4) for ev in evcent]
         self._g.vs["ev_centrality"] = self._evcent_round
 
+        # For visualization
+        layout = self._g.layout('kk')
+        self.visual_style = {}
+        self.visual_style["vertex_size"] = 4
+        self.visual_style["edge_width"] = 0.1
+        self.visual_style["edge_arrow_width"] = 0.1
+        self.visual_style["layout"] = layout
+        
+
 
     def mf_takeup(self, time_period):
         '''
@@ -78,6 +87,9 @@ class Vill_Net:
                 self._g.vs[v]["take_up"] = 1
             else:
                 self._g.vs[v]["take_up"] = 0
+        
+        color_dict = {1: "red", 0: "blue"}
+        self.visual_style["vertex_color"] = [color_dict[take_up] for take_up in self._g.vs["take_up"]]
     
     def plot_graph(self, show = True):
         '''
@@ -86,16 +98,44 @@ class Vill_Net:
         taken up MF
         '''
 
-        layout = self._g.layout('kk')
-        visual_style = {}
-        visual_style["vertex_size"] = 4
-        visual_style["edge_width"] = 0.1
-        visual_style["edge_arrow_width"] = 0.1
-        visual_style["layout"] = layout
-        color_dict = {1: "green", 0: "blue"}
-        visual_style["vertex_color"] = [color_dict[take_up] for take_up in self._g.vs["take_up"]]
         fig, ax = plt.subplots()
         plt.axis('off')
-        igraph.plot(self._g, **visual_style, target=ax)
+        igraph.plot(self._g, **self.visual_style, target=ax)
         if show:
             plt.show()
+    
+    def plot_multiple(self):
+        '''
+        Plot the microfinance take up of 
+        a village for all time periods
+        '''
+
+        self.visual_style["vertex_size"] = 2
+        self.visual_style["edge_width"] = 0.05
+        self.visual_style["edge_arrow_width"] = 0.05
+
+        rows = math.ceil(len(self._p_rate)/3)
+
+        for i in range(len(self._p_rate)):
+
+            self.mf_takeup(i)
+            if i <= 2:
+                j = 0
+                k = i
+            elif i > 2 and i <= 5:
+                j = 1
+                k = i - 3
+            elif i > 5 and i <= 8:
+                j = 2
+                k = i - 6
+            else:
+                j = 3
+                k = i - 9
+            plt.figure(0)
+            ax = plt.subplot2grid((rows, 3), (j, k))
+            plt.axis('off')
+            plt.title(str('time-' + str(i)))
+            igraph.plot(self._g, **self.visual_style, target=ax)
+        
+        plt.show()
+            
